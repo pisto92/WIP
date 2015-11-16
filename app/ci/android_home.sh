730bdd9ff995b1075dea024e6ac5bcb0a88a9f8a
pisto <pisto9228@gmail.com>
2015-11-16 20:52:24 +0100

    script test_release_beta e android_home

---
+++ app/ci/android_home.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export ANDROID_HOME=$HOME/android-sdk
+export ADB=${ANDROID_HOME}/platform-tools/adb
+export DEBUG_APK=../build/outputs/apk/app-debug-unaligned.apk
---
+++ app/ci/build_release_beta.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${CURRENT_DIR}
+source ./android_home.sh
+
+cd ../../
+./gradlew -PtestfairyChangelog="$1" testfairyRelease
