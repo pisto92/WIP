bd2600ccaa2049cc90eb83f406ad2ef1ca0e2bc9
pisto <pisto9228@gmail.com>
2015-11-17 10:15:31 +0100

    app signed con testfairyRelease

--- app/ci/android_home.sh
+++ app/ci/android_home.sh
@@ -2,4 +2,4 @@
 
 export ANDROID_HOME=/home/xpuser/android-sdk
 export ADB=${ANDROID_HOME}/platform-tools/adb
-export DEBUG_APK=../build/outputs/apk/app-debug-unaligned.apk
+export DEBUG_APK=../build/outputs/apk/app-debug.apk
